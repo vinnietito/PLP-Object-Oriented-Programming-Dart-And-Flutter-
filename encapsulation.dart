6e84354fe779b967dfb5a79eab90e4510bb22909
vinnietito <vincentnjuguna69@gmail.com>
2024-11-11 11:44:15 +0300

    Commits

--- encapsulation.dart
+++ encapsulation.dart
@@ -1,13 +1,16 @@
 import 'factoryMethods.dart';
 
 class circle {
-  //Private Pro
-  double_radius;
+  //Private Propert with underscore(_)
+  double _radius;
 
-  Circle(this.radius);
+  //Constructor to initialize the radius
+  Circle(this._radius);
 
+  // Getter method to access the private radius
   double get radius => _radius;
 
+  // Setter method to update the radius with validation
   set radius(double value){
     if(value > 0){
       _radius =  value;
@@ -24,14 +27,21 @@ class circle {
 
 void main() {
   //Creating an instance of Circle
-  Circle circle = circle(5.0);
+  Circle circle = Circle(5.0);
 
+  // Accessing the radius using getter
+  print("Initial radius: ${circle.radius}");
+
+  // Calculating and printing the area
   print("Initial area: ${circle.calculateArea()}");
 
+  //Updating the radius using setter
   circle.radius = 7.0;
 
+  //Accessing updated radius and area 
   print("Updated radius: ${circle.radius}");
   print("Updated area: ${circle.calculateArea()}");
 
+  //Trying to set an invalid radius
   circle.radius = -3.0; //This will triger validation
 }
