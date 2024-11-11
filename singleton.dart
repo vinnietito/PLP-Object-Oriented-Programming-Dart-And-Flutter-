e36469566505cfb0316338f3c92db838f3a9d853
vinnietito <vincentnjuguna69@gmail.com>
2024-11-11 19:23:16 +0300

    Commits

--- singleton.dart
+++ singleton.dart
@@ -3,3 +3,10 @@ class FileSystemsManager{
   //Static and final instance variable to hold the single instance of the class
   static final FileSystemsManager_instance = FileSystemsManager._internal();
 }
+//Private constructor to restrict external instantiation
+FileSystemsManager._internal();
+
+//Factory constructor to control the creation of the singleton instance
+factory FileSystemsManager(){
+  return_instance; //Always returns the same instance
+}
