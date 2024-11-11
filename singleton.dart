9498099c780a3394fda57efe60b02382f39d2e2e
vinnietito <vincentnjuguna69@gmail.com>
2024-11-11 19:19:39 +0300

    Commits

---
+++ singleton.dart
@@ -0,0 +1,5 @@
+//Singleton class to manage file systems operations
+class FileSystemsManager{
+  //Static and final instance variable to hold the single instance of the class
+  static final FileSystemsManager_instance = FileSystemsManager._internal();
+}
