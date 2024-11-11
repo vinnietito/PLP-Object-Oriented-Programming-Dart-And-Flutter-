df50bc35e7800cdeb330aa97235ac627234c6043
vinnietito <vincentnjuguna69@gmail.com>
2024-11-11 11:07:50 +0300

    Commits

--- fruit.dart
+++ fruit.dart
@@ -6,5 +6,11 @@ class Fruit {
 
   Fruit(this.color, this.fruitType, this.taste)
 
-  
+  void eatFruit() {
+    print("You are ${fruitType}, that is in ${color}, tastes ${taste}, it costed me this much ${cost}");
+  }
+}
+
+void main(){
+  Fruit apple = Fruit("green", "citrus", "sour");
 }
