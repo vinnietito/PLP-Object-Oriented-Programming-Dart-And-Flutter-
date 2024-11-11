0f0529b4ff1380edc618a2369432c8671d5f6e7d
vinnietito <vincentnjuguna69@gmail.com>
2024-11-11 19:59:45 +0300

    Commits

--- extension.dart
+++ extension.dart
@@ -1,1 +1,11 @@
 //Extending the String class with an extension method
+extension StringExtensions on String {
+  //Method to capitalize the first letter of a string
+  String capitalizeFirstLetter() {
+    if(this.isEmpty) {
+      return this;
+    }
+
+    return this[0].toUpperCase() + this.substring(1);
+  }
+}
